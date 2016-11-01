e14f261526159db27d86cff994027edcff620afe
Fekri <m.fekri@roundtableapps.com>
2016-11-01 17:13:58 +0330

    initialize

---
+++ MFSlider/MFSlider/MFSlider.h
@@ -0,0 +1,19 @@
+//
+//  MFSlider.h
+//  MFSlider
+//
+//  Created by Fekri on 11/1/16.
+//  Copyright © 2016 MF. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for MFSlider.
+FOUNDATION_EXPORT double MFSliderVersionNumber;
+
+//! Project version string for MFSlider.
+FOUNDATION_EXPORT const unsigned char MFSliderVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <MFSlider/PublicHeader.h>
+
+
